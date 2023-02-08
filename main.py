4c9b93f125f93c30d613d8d10992cf9e0fd7a24a
Brent Young <bryoung777@icloud.com>
2023-02-07 16:09:03 -0800

    Attempt 1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     ##################################################
     # Comlete your code here
+    C = int(input("Enter Calcius: "))
+    F = (9 / 5) * C + 32
+    print(f'Farenheit: \t {F:.2f}')
     ##################################################
     pass
 
